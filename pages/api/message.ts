c79c1eb1ce2487b16c7ea2c3a2ff373c35e3fb05
mploythai <110638329+mploythai@users.noreply.github.com>
2023-03-08 14:20:12 -0800

    Ported message sending api

---
+++ pages/api/message.ts
@@ -0,0 +1,62 @@
+import validationSchema from "@/lib/validationSchema";
+import { FormikErrors, FormikValues } from "formik";
+import { NextApiRequest, NextApiResponse } from "next";
+import nodemailer from "nodemailer";
+import sanitize from "sanitize-html";
+
+export default async function MessageSender(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const transporter = nodemailer.createTransport({
+    service: process.env.NEXT_PUBLIC_MAIL_SERVICE,
+    auth: {
+      user: process.env.NEXT_PUBLIC_MAIL_USER,
+      pass: process.env.NEXT_PUBLIC_MAIL_PASS,
+    },
+  });
+
+  const safeText = () => {
+    Object.keys(req.body).forEach((key) => {
+      req.body[key] = sanitize(req.body[key], {
+        disallowedTagsMode: "escape",
+        allowedTags: [],
+      });
+    });
+
+    return req.body;
+  };
+
+  const { name, email, subject, message } = safeText();
+
+  try {
+    await validationSchema.validate({ name, email, subject, message });
+
+    if (await validationSchema.isValid({ name, email, subject, message })) {
+      await transporter.sendMail({
+        from: `"${name}" <${email}>`,
+        to: `${process.env.NEXT_PUBLIC_MAIL_USER}`,
+        subject: `${subject}`,
+        text: `${message}`,
+        html: `
+          <p>
+            From ${name},
+            <br><br>
+            "${message}"
+            <br><br>
+            <a href="mailto:${email}?subject=RE:${subject}">
+              Click here to reply back to them! (${email})
+            </a>
+          </p>
+        `,
+      });
+
+      res.status(200).setHeader("Content-Type", "application/json");
+    }
+  } catch (error) {
+    const e = error as FormikErrors<FormikValues>;
+    res.status(400).json(e.errors);
+  }
+
+  res.end();
+}
