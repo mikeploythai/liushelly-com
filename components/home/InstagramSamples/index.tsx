985d1ed9aa0cebcd84f708ad2ab157294b0d5c14
mploythai <110638329+mploythai@users.noreply.github.com>
2023-03-12 14:17:34 -0700

    Changed component to be wrapped with Card

--- components/home/InstagramSamples/index.tsx
+++ components/home/InstagramSamples/index.tsx
@@ -1,4 +1,6 @@
 import {
+  Card,
+  CardBody,
   Container,
   Flex,
   Heading,
@@ -26,28 +28,25 @@ const InstagramSamples = () => {
         <SimpleGrid columns={[2, 4]} gap={4}>
           {instagramSampleData.map(({ title, link, img }, index) => {
             return (
-              <Link
+              <Card
                 key={index}
+                as={Link}
                 href={link}
                 referrerPolicy="strict-origin-when-cross-origin"
-                filter="auto"
-                _hover={{ brightness: "87.5%" }}
-                _active={{ brightness: "75%" }}
                 isExternal
               >
-                <Image
-                  src={img}
-                  alt={title}
-                  height={1350}
-                  width={1080}
-                  quality={50}
-                  sizes="(max-width: 480px) 40vw, 60vw"
-                  style={{
-                    objectFit: "cover",
-                    border: "1px solid var(--chakra-colors-brand-dark)",
-                  }}
-                />
-              </Link>
+                <CardBody minH="unset">
+                  <Image
+                    src={img}
+                    alt={title}
+                    height={1350}
+                    width={1080}
+                    quality={50}
+                    sizes="(max-width: 480px) 40vw, 60vw"
+                    style={{ objectFit: "cover" }}
+                  />
+                </CardBody>
+              </Card>
             );
           })}
         </SimpleGrid>
