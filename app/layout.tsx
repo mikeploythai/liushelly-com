18f818beb1ee0fd1a0e229a101dd259af4d93e3f
mploythai <110638329+mploythai@users.noreply.github.com>
2023-03-23 11:27:20 -0700

    Moved metadata back out smh

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,11 @@
 import { Analytics } from "@vercel/analytics/react";
 
+export const metadata = {
+  title: "Shelly Liu, Social Media Manager",
+  description:
+    "Shelly Liu is a professional social media manager and digital marketing expert based in southern California.",
+};
+
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
