5a7354410e2768a5ef781cce24cf8479f1f1e7b4
mploythai <110638329+mploythai@users.noreply.github.com>
2023-03-24 23:54:33 -0700

    Added basic page animations

--- app/template.tsx
+++ app/template.tsx
@@ -5,6 +5,7 @@ import NavBar from "@/components/common/NavBar";
 import theme from "@/theme";
 import { CacheProvider } from "@chakra-ui/next-js";
 import { ChakraBaseProvider, VStack } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 
 const Template = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -12,7 +13,14 @@ const Template = ({ children }: { children: React.ReactNode }) => {
       <ChakraBaseProvider theme={theme} resetCSS>
         <VStack minH="100vh" justify="space-between" spacing={0}>
           <NavBar />
-          {children}
+          <motion.div
+            initial={{ opacity: 0, y: 16 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 16 }}
+            style={{ width: "100%" }}
+          >
+            {children}
+          </motion.div>
           <Footer />
         </VStack>
       </ChakraBaseProvider>
