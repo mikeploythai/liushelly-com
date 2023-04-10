903dba29ef557ab45de9b3f40d5c2025fea32f46
mploythai <110638329+mploythai@users.noreply.github.com>
2023-04-10 12:50:53 -0700

    Added more data

--- lib/data/servicesData.ts
+++ lib/data/servicesData.ts
@@ -1,7 +1,12 @@
 const servicesData = [
   {
     title: "social media management",
+    description: `This is my flagship service, where I get hands-on and take charge of your social media platforms!
+
+    With four different tiers to choose from, you can select the one that's best suited for your business's budget and specific needs. Each tier offers different levels of account management, post creation, and audience engagement, ensuring maximum flexibility and customization.`,
     img: "https://images.pexels.com/photos/606505/pexels-photo-606505.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    url: "https://calendly.com/shellyliu/30mindiscoverychat",
+    cta: "work with me",
     inclusions: [
       "Personalized content + growth strategy",
       "Graphics creation and caption writing",
@@ -15,19 +20,35 @@ const servicesData = [
   },
   {
     title: "instagram audits",
+    description: `Need help optimizing your Instagram page?
+
+    Let me take a look at your profile and analyze your profile picture, bio, story highlights, and content! Then, I'll compile a personalized guide for you that lets you know what you're doing great, what needs improvement, what changes I would make, and how to implement them.`,
     img: "https://images.pexels.com/photos/15014909/pexels-photo-15014909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    url: "/",
+    cta: "get your audit",
   },
   {
     title: "1-on-1 strategy calls",
+    description: `Spend a full 90 minutes with me! We'll dive deep into your social media challenges, auditing the strengths and weaknesses of your profiles, content, and captions, then I'll help you plan out content pillars, find the best hashtags for you to use to maximize your outreach and engagement, and optimize your profiles.
+
+    You'll also receive a personalized step-by-step guide summarizing the improvements, content strategy, and growth strategy we discussed!`,
     img: "https://images.pexels.com/photos/1449462/pexels-photo-1449462.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    url: "/",
+    cta: "book a call",
   },
   {
     title: "branded instagram templates",
-    img: "https://images.pexels.com/photos/1449462/pexels-photo-1449462.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    description: `TODO`,
+    img: "https://images.pexels.com/photos/60202/pexels-photo-60202.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    url: "/",
+    cta: "request your templates",
   },
   {
     title: "blog writing",
-    img: "https://images.pexels.com/photos/1449462/pexels-photo-1449462.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    description: `TODO`,
+    img: "https://images.pexels.com/photos/1408199/pexels-photo-1408199.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    url: "/",
+    cta: "IDK A GOOD CTA FOR THIS",
   },
 ];
 
