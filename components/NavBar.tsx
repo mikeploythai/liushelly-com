2238d0cee06b7793ff9b8c9d2af1679aa45e0540
mploythai <110638329+mploythai@users.noreply.github.com>
2023-04-29 21:55:27 -0700

    Replacated functionality from previous build

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,53 +1,30 @@
 import { Unbounded } from "next/font/google";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import NavMenu from "./NavMenu";
+import NavRoutes from "./NavRoutes";
 
 const logoFont = Unbounded({ subsets: ["latin"] });
 
 export default function NavBar() {
-  const routes = ["about", "services", "portfolio", "contact"];
+  const pathname = usePathname();
 
   return (
-    <header
-      className={`
-        flex sticky top-0 justify-center bg-brand-light border-b border-b-brand-dark
-        dark:bg-brand-dark dark:border-b-brand-light
-      `}
-    >
-      <div className="flex max-w-screen-md w-full justify-between items-center">
+    <header className="flex sticky top-0 justify-center bg-brand-light border-b border-b-brand-dark z-20 dark:bg-brand-dark dark:border-b-brand-light">
+      <div className="flex max-w-screen-lg w-full justify-between items-center">
         <Link
           href="/"
-          className={`
-            ${logoFont.className} p-4 font-semibold text-lg transition ease-in-out
-            hover:bg-brand-dark/10 active:bg-brand-dark/20
-            dark:hover:bg-brand-light/10 dark:active:bg-brand-light/20
-          `}
+          className={`${logoFont.className} p-4 font-semibold text-lg transition ease-in-out hover:bg-brand-dark/10 active:bg-brand-dark/20 dark:hover:bg-brand-light/10 dark:active:bg-brand-light/20`}
+          onClick={() => pathname === "/" && window.scrollTo(0, 0)}
         >
           Shelly Liu
         </Link>
 
-        <nav
-          className={`
-            hidden pr-2.5
-            sm:flex
-          `}
-        >
-          {routes.map((val, index) => {
-            return (
-              <Link
-                key={index}
-                href={`/${val}`}
-                className={`
-                  p-2.5 text-xs uppercase bg-brand-dark text-brand-light transition ease-in-out
-                  hover:bg-brand-dark/90 active:bg-brand-dark/80
-                  dark:text-brand-dark dark:bg-brand-light
-                  dark:hover:bg-brand-light/90 dark:active:bg-brand-light/80
-                `}
-              >
-                {val}
-              </Link>
-            );
-          })}
+        <nav className="hidden pr-2.5 sm:flex">
+          <NavRoutes />
         </nav>
+
+        <NavMenu />
       </div>
     </header>
   );
