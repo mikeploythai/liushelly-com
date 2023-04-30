5ca8a72c692cfe93352c25e4f820fb5c1f004235
mploythai <110638329+mploythai@users.noreply.github.com>
2023-04-29 21:52:50 -0700

    Added some more custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,18 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        sm: "480px",
+      },
       colors: {
         brand: {
           light: "#d2cbff",
           dark: "#242f78",
         },
+      },
+      boxShadow: {
+        normal: "0.5rem 0.5rem 0 rgb(0, 0, 0)",
+        hover: "0.75rem 0.75rem 0 rgb(0, 0, 0)",
       },
     },
   },
