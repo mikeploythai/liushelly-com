10b83dbbc4709cabed67f6bd5f674426b586786e
mploythai <110638329+mploythai@users.noreply.github.com>
2023-06-02 21:15:33 -0700

    Unused/deleted things I forgot to commit earlier

--- app/contact/layout.tsx
+++
@@ -1,7 +0,0 @@
-export const metadata = {
-  title: "CONTACT",
-};
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
-}
--- app/services/layout.tsx
+++
@@ -1,7 +0,0 @@
-export const metadata = {
-  title: "SERVICES",
-};
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
-}
