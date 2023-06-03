50f5d716309b3387b3ae3b172a53a0af9a5cdcb3
mploythai <110638329+mploythai@users.noreply.github.com>
2023-06-02 20:59:33 -0700

    Imports are no longer async + Sanity integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,13 @@
+import Footer from "@/components/navigation/Footer";
+import NavBar from "@/components/navigation/NavBar";
+import NavDrawer from "@/components/navigation/NavBar/NavDrawer";
+import sanity from "@/lib/sanity/client";
+import { groq } from "next-sanity";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 
 const bodyFont = Montserrat({ subsets: ["latin"] });
+const query = groq`*[_type=="socials"] | order(orderRank)`;
 
 export const metadata = {
   title: {
@@ -12,15 +18,21 @@ export const metadata = {
     "Shelly Liu is a social media manager based in southern California.",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const socialData = await sanity.fetch(query);
+
   return (
     <html lang="en">
       <body className={`${bodyFont.className} flex flex-col min-h-screen`}>
+        <NavBar>
+          <NavDrawer socialData={socialData} />
+        </NavBar>
         {children}
+        <Footer socialData={socialData} />
       </body>
     </html>
   );
