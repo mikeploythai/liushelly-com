26c8f320b7f8bf30d4f57c43a1257d3f1a967be2
mploythai <110638329+mploythai@users.noreply.github.com>
2023-06-02 21:13:17 -0700

    Minor edits

--- components/navigation/Footer.tsx
+++ components/navigation/Footer.tsx
@@ -1,52 +1,36 @@
-import sanity from "@/lib/sanityClient";
-import { SocialProps } from "@/lib/socialProps";
-import { groq } from "next-sanity";
-import { Unbounded } from "next/font/google";
+import { primaryFont } from "@/lib/primaryFont";
+import { Social } from "@/lib/types";
 import Link from "next/link";
-import Socials from "../shared/Socials";
+import Socials from "./Socials";
 
-const logoFont = Unbounded({ subsets: ["latin"] });
-
-const query = groq`
-  *[_type == "socials"] | order(orderRank) {
-    platform,
-    link
-  }
-`;
-
-export default async function Footer() {
-  const data: SocialProps[] = await sanity.fetch(query);
+export default function Footer({ socialData }: { socialData: Social[] }) {
   const date = new Date();
   const year = date.getFullYear();
 
   return (
-    <footer className="flex justify-center">
-      <div className="flex flex-col gap-8 max-w-screen-md w-full items-center justify-between mx-12 py-16 sm:flex-row">
-        <div className="flex flex-col gap-2 items-center sm:items-start">
-          <p
-            className={`${logoFont.className} text-base font-semibold text-center`}
+    <footer className="flex flex-col max-w-screen-md w-full items-center justify-between mx-auto p-16 gap-4 sm:flex-row">
+      <div className="flex flex-col items-center gap-1.5 sm:items-start">
+        <h3 className={`${primaryFont.className} font-semibold text-base`}>
+          &copy; Shelly Liu {year}
+        </h3>
+
+        <small className="text-[10px] text-center font-medium">
+          <Link href="/privacy" className="hover:underline">
+            Privacy Policy
+          </Link>{" "}
+          &middot;{" "}
+          <a
+            href="https://mikeploythai.com"
+            target="_blank"
+            referrerPolicy="strict-origin-when-cross-origin"
+            className="hover:underline"
           >
-            &copy; Shelly Liu {year}
-          </p>
-
-          <small className="text-[10px] font-medium">
-            <Link href="/privacy" className="hover:underline">
-              Privacy Policy
-            </Link>{" "}
-            &middot;{" "}
-            <a
-              href="https://mikeploythai.com"
-              target="_blank"
-              referrerPolicy="strict-origin-when-cross-origin"
-              className="hover:underline"
-            >
-              Built by Mike
-            </a>
-          </small>
-        </div>
-
-        <Socials data={data} />
+            Built by Mike
+          </a>
+        </small>
       </div>
+
+      <Socials data={socialData} />
     </footer>
   );
 }
