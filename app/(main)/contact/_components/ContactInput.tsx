45042960600d16fd6f9f4ac80fe2c79512b1085e
mploythai <110638329+mploythai@users.noreply.github.com>
2023-06-07 23:20:11 -0700

    Fixed label error

--- app/(main)/contact/_components/ContactInput.tsx
+++ app/(main)/contact/_components/ContactInput.tsx
@@ -10,29 +10,29 @@ export default function ContactInput({ label, ...props }: ContactInputProps) {
   const [field, meta] = useField(props);
 
   return (
-    <div className="flex flex-col gap-1.5">
-      <label htmlFor={props.name} className="text-sm font-medium">
+    <>
+      <label className="flex flex-col gap-1.5 text-sm font-medium">
         {label}
-      </label>
 
-      {props.name != "message" ? (
-        <Field
-          {...field}
-          {...props}
-          className="p-2.5 text-sm border border-brand-dark rounded-none focus:outline-none"
-        />
-      ) : (
-        <Field
-          as="textarea"
-          {...field}
-          {...props}
-          className="h-32 p-2.5 text-sm border border-brand-dark rounded-none focus:outline-none"
-        />
-      )}
+        {props.name != "message" ? (
+          <Field
+            {...field}
+            {...props}
+            className="p-2.5 text-sm border border-brand-dark rounded-none focus:outline-none"
+          />
+        ) : (
+          <Field
+            as="textarea"
+            {...field}
+            {...props}
+            className="h-32 p-2.5 text-sm border border-brand-dark rounded-none focus:outline-none"
+          />
+        )}
+      </label>
 
       {meta.touched && meta.error && (
         <small className="text-red-700 text-xs">{meta.error}</small>
       )}
-    </div>
+    </>
   );
 }
