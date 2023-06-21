e32e036effa9e03bc2888b50f96507ad5ba01988
mploythai <110638329+mploythai@users.noreply.github.com>
2023-06-21 14:17:03 -0700

    Metadata stuff

---
+++ app/sitemap.ts
@@ -0,0 +1,26 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://liushelly.com",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://liushelly.com/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://liushelly.com/services",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://liushelly.com/portfolio",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://liushelly.com/contact",
+      lastModified: new Date(),
+    },
+  ];
+}
