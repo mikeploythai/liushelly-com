5d322c819c7a5df02826e48af1cf798319756582
mploythai <110638329+mploythai@users.noreply.github.com>
2023-11-02 02:10:53 -0700

    Create scrolling banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { IconArrowUpRight } from "@tabler/icons-react";
 import Image from "next/image";
+import { Fragment } from "react";
 import { buttonVariants } from "./_components/ui/button";
 
 export default function HomePage() {
@@ -31,6 +32,39 @@ export default function HomePage() {
             height={432}
             className="h-full bg-violet-50"
           />
+        </div>
+      </section>
+
+      <section
+        role="marquee"
+        aria-label="Scrolling banner"
+        className="group bg-indigo-950 py-3"
+      >
+        <div className="relative mx-auto flex max-w-screen-2xl overflow-x-hidden font-heading font-medium uppercase text-violet-200">
+          <div className="whitespace-nowrap ease-linear animate-out slide-out-to-left duration-60s repeat-infinite group-hover:paused">
+            {Array(5)
+              .fill("Bookings for winter 2023 are now open")
+              .map((text, key) => (
+                <Fragment key={key}>
+                  <span className="mx-3">{text}</span>
+                  <span className="mx-3">✨</span>
+                </Fragment>
+              ))}
+          </div>
+
+          <div className="absolute whitespace-nowrap ease-linear animate-in slide-in-from-right duration-60s repeat-infinite group-hover:paused">
+            {Array(5)
+              .fill("Bookings for winter 2023 are now open")
+              .map((text, key) => (
+                <Fragment key={key}>
+                  <span className="mx-3">{text}</span>
+                  <span className="mx-3">✨</span>
+                </Fragment>
+              ))}
+          </div>
+
+          <span className="absolute left-0 hidden h-full w-1/6 bg-gradient-to-r from-indigo-950 to-transparent 2xl:block" />
+          <span className="absolute right-0 hidden h-full w-1/6 bg-gradient-to-l from-indigo-950 to-transparent 2xl:block" />
         </div>
       </section>
     </main>
