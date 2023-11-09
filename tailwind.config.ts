c4538efb026a3396cf118a33bb79b1068bbf3d2f
mploythai <110638329+mploythai@users.noreply.github.com>
2023-11-09 14:41:58 -0800

    Added TW typography

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -39,5 +39,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 } satisfies Config;
