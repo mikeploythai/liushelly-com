b591573be397348e36dee0ecd145df2b66500745
mploythai <110638329+mploythai@users.noreply.github.com>
2023-11-09 23:14:28 -0800

    Fixed interface

--- src/app/_components/markdown-wrapper.tsx
+++ src/app/_components/markdown-wrapper.tsx
@@ -1,28 +1,26 @@
 import { forwardRef } from "react";
 import { cn } from "~/lib/cn";
 
-interface MarkdownHTMLAttributes<T>
-  extends React.HTMLAttributes<HTMLDivElement> {
+interface MarkdownHTMLAttributes extends React.HTMLAttributes<HTMLDivElement> {
   theme?: "default" | "secondary";
 }
 
-const MarkdownWrapper = forwardRef<
-  HTMLDivElement,
-  MarkdownHTMLAttributes<HTMLDivElement>
->(({ theme = "default", className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn(
-      "prose prose-sm max-w-none space-y-3 md:prose-base prose-headings:font-heading prose-headings:font-medium prose-blockquote:border-none prose-blockquote:p-0 prose-blockquote:font-normal",
-      theme === "default" &&
-        "text-indigo-950 prose-headings:text-indigo-950 prose-a:text-indigo-950 prose-blockquote:text-indigo-950",
-      theme === "secondary" &&
-        "text-violet-200 prose-headings:text-violet-200 prose-a:text-violet-200 prose-blockquote:text-violet-200",
-      className,
-    )}
-    {...props}
-  />
-));
+const MarkdownWrapper = forwardRef<HTMLDivElement, MarkdownHTMLAttributes>(
+  ({ theme = "default", className, ...props }, ref) => (
+    <div
+      ref={ref}
+      className={cn(
+        "prose prose-sm max-w-none space-y-3 md:prose-base prose-headings:font-heading prose-headings:font-medium prose-blockquote:border-none prose-blockquote:p-0 prose-blockquote:font-normal",
+        theme === "default" &&
+          "text-indigo-950 prose-headings:text-indigo-950 prose-a:text-indigo-950 prose-blockquote:text-indigo-950",
+        theme === "secondary" &&
+          "text-violet-200 prose-headings:text-violet-200 prose-a:text-violet-200 prose-blockquote:text-violet-200",
+        className,
+      )}
+      {...props}
+    />
+  ),
+);
 MarkdownWrapper.displayName = "MarkdownWrapper";
 
 export default MarkdownWrapper;
