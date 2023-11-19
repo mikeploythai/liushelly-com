2051e4362712008ea82eb87bc0aeb23e68f39716
mploythai <110638329+mploythai@users.noreply.github.com>
2023-11-18 19:18:32 -0800

    Created service page layout

--- src/app/(public)/services/[slug]/page.tsx
+++ src/app/(public)/services/[slug]/page.tsx
@@ -1,13 +1,135 @@
-import PageWrapper from "~/app/(public)/_components/page-wrapper";
+import Link from "next/link";
+import { client } from "sanity-studio/lib/client";
+import type { ListItem } from "sanity-studio/types";
 
-export default function ServicePage({
+import { PortableText } from "@portabletext/react";
+import { IconArrowUpRight, IconChevronLeft } from "@tabler/icons-react";
+import { groq } from "next-sanity";
+import Image from "next/image";
+import { sanityImage } from "sanity-studio/lib/image";
+import { BlockImage, BlockLink } from "sanity-studio/portable-text/components";
+import PageWrapper from "~/app/(public)/_components/page-wrapper";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "~/app/(public)/_components/ui/tabs";
+import { cn } from "~/lib/cn";
+import MarkdownWrapper from "../../_components/markdown-wrapper";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "../../_components/ui/accordion";
+import { buttonVariants } from "../../_components/ui/button";
+
+export default async function ServicePage({
   params: { slug },
 }: {
   params: { slug: string };
 }) {
+  const service: ListItem = await client.fetch(query, {
+    slug,
+    next: {
+      cache: "no-store",
+    },
+  });
+
+  if (!service) return;
+
   return (
-    <PageWrapper>
-      <h1>{slug}</h1>
+    <PageWrapper className="mx-auto max-w-screen-md space-y-6 p-6 md:p-12">
+      <Link
+        href="/services"
+        className={cn(buttonVariants({ variant: "link", size: "sm" }), "p-0")}
+      >
+        <IconChevronLeft size={12} />
+        Back to services
+      </Link>
+
+      <h1 className="sr-only">Service details</h1>
+
+      <MarkdownWrapper>
+        <figure className="relative min-h-[208px] sm:aspect-[16/6] sm:min-h-0">
+          <Image
+            src={sanityImage(service.image).format("webp").url()}
+            alt={service.name}
+            placeholder="blur"
+            blurDataURL={service.image.lqip}
+            className="border border-indigo-950 bg-white object-cover"
+            fill
+          />
+        </figure>
+
+        <h2>
+          {service.name.toLowerCase() === "social media management" && (
+            <b className="uppercase underline">Done for you</b>
+          )}{" "}
+          {service.name}
+        </h2>
+
+        <PortableText
+          value={service.content}
+          components={{
+            types: { image: BlockImage },
+            marks: { link: BlockLink },
+          }}
+        />
+      </MarkdownWrapper>
+
+      <Link href={`/`} className={buttonVariants({ class: "w-full" })}>
+        Book a discovery call
+        <IconArrowUpRight size={18} className="ml-auto" />
+      </Link>
+
+      <Tabs defaultValue="packages">
+        <TabsList className="grid grid-cols-2">
+          <TabsTrigger value="packages">Packages</TabsTrigger>
+          <TabsTrigger value="contents">Contents</TabsTrigger>
+        </TabsList>
+
+        <TabsContent value="packages">
+          Make changes to your account here.
+        </TabsContent>
+        <TabsContent value="contents">Change your password here.</TabsContent>
+      </Tabs>
+
+      <h3 className="font-heading text-lg font-medium md:text-xl">FAQ</h3>
+
+      <Accordion type="single" className="!mt-1.5" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+
+        <AccordionItem value="item-2">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+
+        <AccordionItem value="item-3">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </PageWrapper>
   );
 }
+
+const query = groq`
+*[_type == "services" && slug.current == $slug][0] {
+  ...,
+  image {
+    ...,
+    'lqip': asset->metadata.lqip,
+  },
+}
+`;
--- src/app/(public)/services/page.tsx
+++ src/app/(public)/services/page.tsx
@@ -1,7 +1,13 @@
+import type { ListItem } from "sanity-studio/types";
+
+import { PortableText } from "@portabletext/react";
 import { IconChevronRight } from "@tabler/icons-react";
+import { groq } from "next-sanity";
 import Image from "next/image";
 import Link from "next/link";
-import { services } from "~/lib/fake-db";
+import { client } from "sanity-studio/lib/client";
+import { sanityImage } from "sanity-studio/lib/image";
+import { BlockImage, BlockLink } from "sanity-studio/portable-text/components";
 import CardGrid from "../_components/card-grid";
 import MarkdownWrapper from "../_components/markdown-wrapper";
 import Marquee from "../_components/marquee";
@@ -14,7 +20,14 @@ import {
   CardHeader,
 } from "../_components/ui/card";
 
-export default function ServicesPage() {
+export default async function ServicesPage() {
+  const services: ListItem[] = await client.fetch(query, {
+    next: {
+      cache: "no-store",
+    },
+  });
+
+  if (!services) return;
   const servicesList = services.slice(1, services.length);
 
   return (
@@ -26,40 +39,47 @@ export default function ServicesPage() {
 
         <Card className="w-full">
           <CardHeader className="relative min-h-[208px] sm:aspect-[16/6] sm:min-h-0">
-            <Image
-              src=""
-              alt=""
-              className="border border-indigo-950 bg-white"
-              fill
-            />
+            {services[0]?.image && (
+              <Image
+                src={sanityImage(services[0].image).format("webp").url()}
+                alt={services[0].name ?? ""}
+                placeholder="blur"
+                blurDataURL={services[0].image.lqip}
+                className="border border-indigo-950 bg-white object-cover"
+                fill
+              />
+            )}
           </CardHeader>
 
           <CardContent className="border border-dashed border-indigo-950 p-3">
             <MarkdownWrapper className="prose-headings:capitalize">
               <h2>
-                <b>DONE FOR YOU</b>
-                <br></br>
-                social media management
+                {services[0]?.name.toLowerCase() ===
+                "social media management" ? (
+                  <>
+                    <b className="uppercase underline">Done for you</b>{" "}
+                    {services[0].name}
+                  </>
+                ) : (
+                  services[0]?.name
+                )}
               </h2>
 
-              <p>
-                This is my flagship service, where I get hands-on and take
-                charge of your social media platforms!
-              </p>
-
-              <p>
-                With four different tiers to choose from, you can select the one
-                that&apos;s best suited for your business&apos; budget and
-                specific needs. Each tier offers different levels of account
-                management, post creation, and audience engagement, ensuring
-                maximum flexibility and customization.
-              </p>
+              {services[0]?.content && (
+                <PortableText
+                  value={services[0].content}
+                  components={{
+                    types: { image: BlockImage },
+                    marks: { link: BlockLink },
+                  }}
+                />
+              )}
             </MarkdownWrapper>
           </CardContent>
 
           <CardFooter>
             <Link
-              href={services[0]?.link ?? ""}
+              href={`/${services[0]?._type}/${services[0]?.slug.current}`}
               className={buttonVariants({ class: "w-full" })}
             >
               View details
@@ -90,3 +110,13 @@ export default function ServicesPage() {
     </PageWrapper>
   );
 }
+
+const query = groq`
+*[_type == "services"][] {
+  ...,
+  image {
+    ...,
+    'lqip': asset->metadata.lqip,
+  },
+}
+`;
