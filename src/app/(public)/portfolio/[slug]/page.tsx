7f58867daa6a83d828892d3a9d20308b1834cac9
mploythai <110638329+mploythai@users.noreply.github.com>
2023-11-18 17:29:25 -0800

    Fetching data from Sanity

--- src/app/(public)/portfolio/[slug]/page.tsx
+++ src/app/(public)/portfolio/[slug]/page.tsx
@@ -1,13 +1,69 @@
-import PageWrapper from "~/app/(public)/_components/page-wrapper";
+import type { ListItem } from "sanity-studio/types";
 
-export default function BrandPage({
+import { PortableText } from "@portabletext/react";
+import { IconChevronLeft } from "@tabler/icons-react";
+import { groq } from "next-sanity";
+import Link from "next/link";
+import { client } from "sanity-studio/lib/client";
+import { BlockImage, BlockLink } from "sanity-studio/portable-text/components";
+import { cn } from "~/lib/cn";
+import MarkdownWrapper from "../../_components/markdown-wrapper";
+import PageWrapper from "../../_components/page-wrapper";
+import { buttonVariants } from "../../_components/ui/button";
+
+export default async function BrandPage({
   params: { slug },
 }: {
   params: { slug: string };
 }) {
+  const brand: ListItem = await client.fetch(query, {
+    slug,
+    next: {
+      cache: "no-store",
+    },
+  });
+
+  if (!brand) return;
+
   return (
-    <PageWrapper>
-      <h1>{slug}</h1>
+    <PageWrapper className="mx-auto max-w-screen-md space-y-6 p-6 md:p-12">
+      <Link
+        href="/portfolio"
+        className={cn(buttonVariants({ variant: "link", size: "sm" }), "p-0")}
+      >
+        <IconChevronLeft size={12} />
+        Back to portfolio
+      </Link>
+
+      <MarkdownWrapper>
+        <article>
+          <PortableText
+            value={brand.content}
+            components={{
+              types: { image: BlockImage },
+              marks: { link: BlockLink },
+            }}
+          />
+        </article>
+      </MarkdownWrapper>
     </PageWrapper>
   );
 }
+
+const query = groq`
+*[_type == "portfolio" && slug.current == $slug][0] {
+  ...,
+  image {
+    ...,
+    'lqip': asset->metadata.lqip,
+  },
+  content[] {
+    ...,
+    ...select(
+      _type == "image" => {
+        ...,
+        'lqip': asset->metadata.lqip
+      }
+    )
+  }
+}`;
--- src/app/(public)/portfolio/page.tsx
+++ src/app/(public)/portfolio/page.tsx
@@ -1,10 +1,19 @@
-import { portfolio } from "~/lib/fake-db";
+import { client } from "sanity-studio/lib/client";
 import CardGrid from "../_components/card-grid";
 import PageWrapper from "../_components/page-wrapper";
 
-export default function PortfolioPage() {
+export default async function PortfolioPage() {
+  const portfolio = await client.fetch(`*[_type == "portfolio"]`, {
+    type: "portfolio",
+    next: {
+      cache: "no-store",
+    },
+  });
+
+  if (!portfolio) return;
+
   return (
-    <PageWrapper className="mx-auto flex w-full max-w-screen-xl flex-col items-center gap-6 p-6">
+    <PageWrapper className="mx-auto flex w-full max-w-screen-xl flex-col items-center justify-center gap-6 p-6">
       <h1 className="font-heading text-3xl font-medium md:py-6 md:text-center md:text-4xl">
         Brands I&apos;ve worked with!
       </h1>
