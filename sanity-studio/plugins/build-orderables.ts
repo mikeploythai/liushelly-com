051d9850fed2fddce6db0d09952d2cbc5256c1a3
mploythai <110638329+mploythai@users.noreply.github.com>
2023-11-21 14:33:12 -0800

    Created orderable docs helper fcn

---
+++ sanity-studio/plugins/build-orderables.ts
@@ -0,0 +1,28 @@
+import type { ComponentType } from "react";
+import type { DocumentDefinition } from "sanity";
+import type { StructureBuilder, StructureResolverContext } from "sanity/desk";
+
+import { orderableDocumentListDeskItem } from "@sanity/orderable-document-list";
+
+export default function buildOrderables(
+  docs: DocumentDefinition[],
+  S: StructureBuilder,
+  context: StructureResolverContext,
+) {
+  let orderedDocs = [];
+
+  for (const doc of docs) {
+    orderedDocs.push(
+      orderableDocumentListDeskItem({
+        title: doc.title ?? doc.name,
+        id: doc.name,
+        type: doc.name,
+        icon: (doc.icon as ComponentType) ?? undefined,
+        S,
+        context,
+      }),
+    );
+  }
+
+  return orderedDocs;
+}
