b1106ce01378055c8e633bbe2ffe47ad73974189
mploythai <110638329+mploythai@users.noreply.github.com>
2023-11-21 14:34:28 -0800

    Re-added vertical spacing

--- src/app/(public)/_components/markdown-wrapper.tsx
+++ src/app/(public)/_components/markdown-wrapper.tsx
@@ -10,7 +10,7 @@ const MarkdownWrapper = forwardRef<HTMLDivElement, MarkdownHTMLAttributes>(
     <div
       ref={ref}
       className={cn(
-        "prose prose-sm max-w-none md:prose-base prose-headings:font-heading prose-headings:font-medium prose-a:normal-case prose-blockquote:border-none prose-blockquote:p-0 prose-blockquote:font-normal",
+        "prose prose-sm max-w-none space-y-6 md:prose-base prose-headings:font-heading prose-headings:font-medium prose-a:normal-case prose-blockquote:border-none prose-blockquote:p-0 prose-blockquote:font-normal",
         theme === "default" &&
           "text-indigo-950 marker:text-indigo-950 prose-headings:text-indigo-950 prose-a:text-indigo-950 prose-blockquote:text-indigo-950",
         theme === "secondary" &&
