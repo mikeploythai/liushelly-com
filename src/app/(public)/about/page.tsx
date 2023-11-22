01a350260c27f9150a229b0e91b719f5720259b0
mploythai <110638329+mploythai@users.noreply.github.com>
2023-11-22 14:10:27 -0800

    Fetch about data from CMS

--- src/app/(public)/about/page.tsx
+++ src/app/(public)/about/page.tsx
@@ -1,45 +1,34 @@
-import { IconArrowUpRight } from "@tabler/icons-react";
-import ExternalLink from "../_components/external-link";
+import type { About } from "sanity-studio/types";
+
+import { groq } from "next-sanity";
+import { sanityFetch } from "sanity-studio/lib/fetch";
+import ContentBlock from "../_components/content-block";
 import MarkdownWrapper from "../_components/markdown-wrapper";
 import PageWrapper from "../_components/page-wrapper";
 import PhotoGrid from "../_components/photo-grid";
 
-export default function AboutPage() {
+export default async function AboutPage() {
+  const about = await sanityFetch<About>({ query, tags: ["aboutMe"] });
+  if (!about) return;
+
   return (
     <PageWrapper className="mx-auto flex w-full max-w-screen-lg flex-col justify-center gap-6 p-6 md:gap-12 md:p-12">
       <MarkdownWrapper>
-        <h1>Hi, I&apos;m Shelly!</h1>
-
-        <p>
-          I&apos;m a social media manager and digital marketing expert with{" "}
-          <span className="font-semibold">4+ years</span> of experience.
-          I&apos;ve earned my Bachelor&apos;s Degree in Business Administration
-          (Marketing Concentration) from California State University, Fullerton,
-          and current work at{" "}
-          <ExternalLink href="/" className="gap-0.5 capitalize">
-            Technology Therapy Group
-            <IconArrowUpRight size={14} />
-          </ExternalLink>{" "}
-          as an Administrative/Marketing Assistant.
-        </p>
-
-        <p>
-          I also work as a freelance social media manager, strategist, and
-          content writer, prioritizing women-owned, BIPOC-owned, and LGBTQ-owned
-          businesses. I&apos;m super passionate about storytelling through
-          digital marketing, and helping businesses owned by marginalized
-          individuals grow their online presence organically so they can spend
-          more time on their craft rather than on marketing.
-        </p>
-
-        <p>
-          Outside of marketing, I enjoy listening to K-pop and R&B music, going
-          to concerts, reading books and webtoons, thrifting, scrolling through
-          Pinterest, and obsessing over all things Sanrio!
-        </p>
+        <article className="w-full">
+          <ContentBlock content={about.content} />
+        </article>
       </MarkdownWrapper>
 
-      <PhotoGrid width={320} height={480} />
+      <PhotoGrid images={about.images} width={320} height={480} />
     </PageWrapper>
   );
 }
+
+const query = groq`
+*[_type == "aboutMe"][0] {
+  ...,
+  images[] {
+    ...,
+    'lqip': asset->metadata.lqip,
+  }
+}`;
