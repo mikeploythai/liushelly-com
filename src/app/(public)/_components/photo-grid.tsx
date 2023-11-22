b1bfdeaf6c0dd08f7f3d18408d10b9603f39e0bf
mploythai <110638329+mploythai@users.noreply.github.com>
2023-11-22 14:10:06 -0800

    Swapped temp array for image array

--- src/app/(public)/_components/photo-grid.tsx
+++ src/app/(public)/_components/photo-grid.tsx
@@ -1,38 +1,42 @@
+import type { SanityImage } from "sanity-studio/types";
+
 import Image from "next/image";
 import { forwardRef, type HTMLAttributes } from "react";
+import { sanityImage } from "sanity-studio/lib/image";
 import { cn } from "~/lib/cn";
 import { Card, CardContent } from "./ui/card";
 
 interface CardGridProps extends HTMLAttributes<HTMLDivElement> {
+  images: SanityImage[];
   width: number;
   height: number;
 }
 
 const PhotoGrid = forwardRef<HTMLDivElement, CardGridProps>(
-  ({ width, height, className, ...props }, ref) => (
+  ({ images, width, height, className, ...props }, ref) => (
     <div
       ref={ref}
       className={cn("grid grid-cols-2 gap-3 md:grid-cols-4", className)}
       {...props}
     >
-      {Array(4)
-        .fill(null)
-        .map((_, key) => (
-          <Card
-            key={key}
-            className="p-0 md:hover:border-indigo-900 md:hover:shadow-boxy-hover md:hover:!shadow-indigo-900"
-          >
-            <CardContent>
-              <Image
-                src=""
-                alt=""
-                width={width}
-                height={height}
-                className="h-full bg-white"
-              />
-            </CardContent>
-          </Card>
-        ))}
+      {images.map((image, key) => (
+        <Card
+          key={key}
+          className="group p-0 md:hover:border-indigo-900 md:hover:shadow-boxy-hover md:hover:!shadow-indigo-900"
+        >
+          <CardContent className="h-full overflow-hidden">
+            <Image
+              src={sanityImage(image).format("webp").url()}
+              alt=""
+              placeholder="blur"
+              blurDataURL={image.lqip}
+              width={width}
+              height={height}
+              className="h-full bg-white object-cover transition duration-300 ease-in-out md:group-hover:scale-105"
+            />
+          </CardContent>
+        </Card>
+      ))}
     </div>
   ),
 );
