9fbbd79dc1e756f6252bc02d1df80ec93d022630
mploythai <110638329+mploythai@users.noreply.github.com>
2023-11-23 03:57:21 -0800

    Adjusted structure

--- sanity-studio/plugins/structure.ts
+++ sanity-studio/plugins/structure.ts
@@ -1,30 +1,18 @@
 import type { StructureResolver } from "sanity/desk";
 
-import aboutMe from "sanity-studio/schema/about-me";
-import announcement from "sanity-studio/schema/announcement";
-import home from "sanity-studio/schema/home";
-import portfolio from "sanity-studio/schema/portfolio";
-import services from "sanity-studio/schema/services";
-import socials from "sanity-studio/schema/socials";
+import { orderables, singletons } from "sanity-studio/schema";
 import buildOrderables from "./build-orderables";
 import buildSingletons from "./build-singletons";
 
-const singletons = [home, aboutMe, announcement];
-const orderables = [services, portfolio, socials];
-const hidden = [
-  home.name,
-  aboutMe.name,
-  announcement.name,
-  services.name,
-  portfolio.name,
-  socials.name,
-];
+let hidden: string[] = [];
+singletons.forEach(({ name }) => hidden.push(name));
+orderables.forEach(({ name }) => hidden.push(name));
 
 const structure: StructureResolver = (S, context) => {
   const singletonItems = buildSingletons(singletons, S);
   const orderableItems = buildOrderables(orderables, S, context);
   const defaultItems = S.documentTypeListItems().filter(
-    (doc) => !(hidden as string[]).includes(doc.getId()!),
+    (doc) => !hidden.includes(doc.getId()!),
   );
 
   return S.list()
