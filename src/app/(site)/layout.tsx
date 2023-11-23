d50e34d96efd3f5e091d49c923aaa6cf66d4e0c0
mploythai <110638329+mploythai@users.noreply.github.com>
2023-11-23 04:57:16 -0800

    Added new favicon

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -45,7 +45,6 @@ export const metadata = {
     description:
       "Shelly Liu is a freelance social media manager, strategist, and content writer based in Southern California.",
   },
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
 export default function RootLayout({
