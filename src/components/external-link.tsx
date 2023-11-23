ede2703fb23547508365916b655c6013a9294d0a
mploythai <110638329+mploythai@users.noreply.github.com>
2023-11-22 19:42:37 -0800

    Adjusted padding

--- src/components/external-link.tsx
+++ src/components/external-link.tsx
@@ -12,7 +12,7 @@ const ExternalLink = forwardRef<
     ref={ref}
     className={cn(
       buttonVariants({ variant: "link" }),
-      "line-clamp-1 max-w-full p-0",
+      "line-clamp-1 max-w-full p-0.5",
       className,
     )}
     {...props}
