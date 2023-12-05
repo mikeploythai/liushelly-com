de5d565fe60d45b3924438a2a5299b061ef1f691
mploythai <110638329+mploythai@users.noreply.github.com>
2023-12-05 12:09:32 -0800

    Added advanced Iframe config
    - Based on Sanity's Shopify preset

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,12 +4,38 @@
 
 import { visionTool } from "@sanity/vision";
 import { defineConfig } from "sanity";
+import { previewUrl } from "sanity-plugin-iframe-pane/preview-url";
+import defaultDocumentNode from "sanity-studio/plugins/default-document-node";
 import structure from "sanity-studio/plugins/structure";
 import { deskTool } from "sanity/desk";
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
+import type { PreviewSecret } from "~/lib/types";
+
+import {
+  defineUrlResolver,
+  type IframeOptions,
+} from "sanity-plugin-iframe-pane";
 import { clientEnv } from "~/env/client.mjs";
 import { schema } from "./sanity-studio/schema";
+
+import home from "sanity-studio/schema/singletons/home";
+
+const previewDocs = [home.name] satisfies string[];
+const previewSlugDocs = [] satisfies typeof previewDocs;
+const previewBasePathname = "/api/preview";
+
+const urlResolver = defineUrlResolver({
+  base: previewBasePathname,
+  requiresSlug: previewSlugDocs,
+});
+
+const iframeOptions: IframeOptions = {
+  url: urlResolver,
+  urlSecretId: clientEnv.NEXT_PUBLIC_SANITY_PREVIEW_SECRET as PreviewSecret,
+  loader: "Loading preview...",
+  reload: { button: true },
+};
 
 export default defineConfig({
   title: "SL Studio",
@@ -19,9 +45,17 @@ export default defineConfig({
   // Add and edit the content schema in the './sanity/schema' folder
   schema,
   plugins: [
-    deskTool({ structure }),
+    deskTool({ structure, defaultDocumentNode }),
     // Vision is a tool that lets you query your content with GROQ in the studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({ defaultApiVersion: clientEnv.NEXT_PUBLIC_SANITY_API_VERSION }),
+    previewUrl({
+      base: previewBasePathname,
+      requiresSlug: previewSlugDocs,
+      urlSecretId: clientEnv.NEXT_PUBLIC_SANITY_PREVIEW_SECRET as PreviewSecret,
+      matchTypes: previewDocs,
+    }),
   ],
 });
+
+export { iframeOptions, previewDocs };
