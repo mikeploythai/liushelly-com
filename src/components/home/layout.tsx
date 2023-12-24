579f63087333c6f0ca5a56a9b1999d6cbcaed4d9
mikeploythai <110638329+mikeploythai@users.noreply.github.com>
2023-12-23 17:48:22 -0800

    Designed a new hero component

--- src/components/home/layout.tsx
+++ src/components/home/layout.tsx
@@ -6,10 +6,12 @@ import Link from "next/link";
 import { sanityImage } from "sanity-studio/lib/image";
 import { cn } from "~/lib/cn";
 import AnnouncementBanner from "../announcement-banner";
+import BlockContent from "../block-content";
 import CardGrid from "../card-grid";
 import ExternalLink from "../external-link";
 import PageWrapper from "../page-wrapper";
 import PhotoGrid from "../photo-grid";
+import MarkdownProvider from "../providers/markdown";
 import { buttonVariants } from "../ui/button";
 import Testimonials from "./testimonials";
 
@@ -18,47 +20,51 @@ export default function HomeLayout({ data }: { data: HomeData }) {
 
   return (
     <PageWrapper>
-      <section className="relative mx-auto flex w-full max-w-screen-lg flex-col items-center justify-between gap-6 sm:flex-row sm:p-6">
-        <div className="absolute bottom-0 space-y-6 p-6 sm:relative sm:p-0">
-          <h1 className="font-heading text-3xl font-medium text-violet-200 sm:text-indigo-950 md:text-4xl">
-            {home.hero?.heading ?? "Add a heading"}
-          </h1>
+      <section className="mx-auto w-full max-w-screen-lg sm:p-6">
+        <div className="grid overflow-hidden sm:grid-cols-3 sm:rounded-xl sm:border sm:border-indigo-950">
+          <div className="space-y-6 p-6 sm:col-span-2 sm:space-y-12 sm:p-12">
+            <hgroup className="space-y-2 sm:space-y-4">
+              <h1 className="font-heading text-3xl font-medium md:text-4xl">
+                {home.hero?.heading ?? "Add a heading"}
+              </h1>
 
-          <ExternalLink
-            href={home.hero?.cta?.href}
-            className={cn(
-              buttonVariants({ size: "lg" }),
-              "bg-violet-200 text-indigo-950 no-underline hover:bg-indigo-100 hover:text-indigo-950 focus-visible:bg-indigo-100 focus-visible:text-indigo-950",
-              "sm:bg-indigo-950 sm:text-violet-200 sm:hover:bg-indigo-900 sm:hover:text-violet-200 sm:focus-visible:bg-indigo-900 sm:focus-visible:text-violet-200",
-            )}
-          >
-            {!home.hero?.cta?.text
-              ? !home.hero?.cta?.href
-                ? "Add a link and label"
-                : "Add a label"
-              : home.hero.cta.text}
-            <IconArrowUpRight size={20} />
-          </ExternalLink>
+              <MarkdownProvider>
+                <BlockContent content={home.hero?.subheading} />
+              </MarkdownProvider>
+            </hgroup>
+
+            <ExternalLink
+              href={home.hero?.cta?.href}
+              className={buttonVariants({
+                size: "lg",
+                className: "no-underline",
+              })}
+            >
+              {!home.hero?.cta?.text
+                ? !home.hero?.cta?.href
+                  ? "Add a link and label"
+                  : "Add a label"
+                : home.hero.cta.text}
+              <IconArrowUpRight size={20} />
+            </ExternalLink>
+          </div>
+
+          <figure className="relative min-h-64 border-t border-indigo-950 bg-white sm:border-l sm:border-t-0">
+            <Image
+              src={
+                home.hero?.image
+                  ? sanityImage(home.hero.image).url()
+                  : "https://placekitten.com/288/432"
+              }
+              alt={home.hero?.image?.alt ?? "Placekitten"}
+              placeholder={home.hero?.image?.asset ? "blur" : "empty"}
+              blurDataURL={home.hero?.image?.lqip}
+              className="object-cover object-[50%_30%]"
+              priority
+              fill
+            />
+          </figure>
         </div>
-
-        <figure className="group relative -z-10 w-full overflow-hidden border-indigo-950 transition duration-300 ease-in-out sm:z-0 sm:mb-2 sm:mr-2 sm:max-w-fit sm:border sm:shadow-boxy md:hover:border-indigo-900 md:hover:shadow-boxy-hover md:hover:shadow-indigo-900">
-          <Image
-            src={
-              home.hero?.image
-                ? sanityImage(home.hero.image).url()
-                : "https://placekitten.com/288/432"
-            }
-            alt={home.hero?.image?.alt ?? "Placekitten"}
-            placeholder={home.hero?.image?.asset ? "blur" : "empty"}
-            blurDataURL={home.hero?.image?.lqip}
-            width={288}
-            height={432}
-            className="h-full w-full bg-white object-cover transition duration-300 ease-in-out sm:w-72 md:group-hover:scale-105"
-            priority
-          />
-
-          <span className="absolute inset-0 bg-gradient-to-tr from-indigo-950 from-15% to-transparent sm:hidden" />
-        </figure>
       </section>
 
       <AnnouncementBanner announcement={announcement} />
