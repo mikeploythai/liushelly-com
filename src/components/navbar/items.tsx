5ca6c4a3561885dd4cc43198866f571c572682c4
mikeploythai <110638329+mikeploythai@users.noreply.github.com>
2023-12-24 23:09:28 -0800

    swapped sheet close for drawer close component, removed text size overwrite

--- src/components/navbar/items.tsx
+++ src/components/navbar/items.tsx
@@ -5,14 +5,14 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { cn } from "~/lib/cn";
 import { buttonVariants } from "../ui/button";
-import { SheetClose } from "../ui/sheet";
+import { DrawerClose } from "../ui/drawer";
 
 export default function NavbarItems({ isDrawer }: { isDrawer: boolean }) {
   const pathname = usePathname();
   const routes: string[] = ["about", "services", "portfolio", "contact"];
 
   function ItemWrapper({ children }: { children: React.ReactNode }) {
-    if (isDrawer) return <SheetClose asChild>{children}</SheetClose>;
+    if (isDrawer) return <DrawerClose asChild>{children}</DrawerClose>;
     else return <>{children}</>;
   }
 
@@ -21,7 +21,7 @@ export default function NavbarItems({ isDrawer }: { isDrawer: boolean }) {
       className={cn(
         "my-auto",
         !isDrawer && "hidden pr-1.5 sm:block",
-        isDrawer && "flex flex-col",
+        isDrawer && "mt-4 flex flex-col",
       )}
     >
       {routes.map((route) => (
@@ -31,7 +31,7 @@ export default function NavbarItems({ isDrawer }: { isDrawer: boolean }) {
             className={cn(
               buttonVariants({ size: !isDrawer ? "default" : "lg" }),
               pathname === `/${route}` && "bg-indigo-900",
-              isDrawer && "justify-start text-xl",
+              isDrawer && "justify-start",
               "rounded-none sm:first-of-type:rounded-l-sm sm:last-of-type:rounded-r-sm",
             )}
           >
