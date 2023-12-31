16b70ba11af0588a3f8b4babac7de734d302f19f
mikeploythai <110638329+mikeploythai@users.noreply.github.com>
2023-12-30 17:18:35 -0800

    get secret directly from process.env

--- src/app/(site)/api/get-year/route.ts
+++ src/app/(site)/api/get-year/route.ts
@@ -1,11 +1,10 @@
 import { revalidateTag } from "next/cache";
 import { type NextRequest } from "next/server";
-import { serverEnv } from "~/env/server.mjs";
 
 export function GET(req: NextRequest) {
   const authHeader = req.headers.get("authorization");
 
-  if (authHeader !== `Bearer ${serverEnv.CRON_SECRET}`)
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`)
     return new Response("Unauthorized", { status: 401 });
 
   const year = new Date().getFullYear();
