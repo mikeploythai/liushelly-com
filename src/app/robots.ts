bed7aaf6780e903d80b0d966d7a3b5ec99c1cdff
mikeploythai <110638329+mikeploythai@users.noreply.github.com>
2023-12-30 19:00:31 -0800

    disallow subroutes to studio too

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: "/studio/",
+      disallow: "/studio/*",
     },
     sitemap: "https://liushelly.com/sitemap.xml",
   };
